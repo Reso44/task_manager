b4e57b3807f311e530faec2c864440a73a028d76
JoshuaDRose <joshuarose099@gmail.com>
2023-10-28 14:55:14 +1100

    test(task): add more tests for task name

--- cls/__init__.py
+++ cls/__init__.py
@@ -5,9 +5,3 @@ Email: joshuarose@gmx.com
 Github: https://github.com/Reso44
 Description: constructor flie for cls
 """
-
-import os
-import sys
-
-if __name__ == "__main__":
-    sys.path.insert(0, os.path.join(os.getcwd(), "cls"))
