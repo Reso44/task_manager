4f0234a18075e7b2b1dc5189da3aef92d5918905
JoshuaDRose <joshuarose099@gmail.com>
2023-10-28 14:55:14 +1100

    test(task): add more tests for task name

--- cls/task.py
+++ cls/task.py
@@ -16,24 +16,73 @@ class Task:
     ----------------
     TID : uuid.UUID
         A unique Task ID that is randomly generated
+    name : str
+        A short task name that is less than 15 characters
+
+    Usage
+    -----
+
+    The Task class can be instanced as follows:
+    >>> my_task = Task("Clean my room")
+    >>> isinstance(my_task, Task)
+    True
     """
 
-    def __init__(self):
+    def __init__(self, name):
         """
         Parameters
         ----------
         _task_id : uuid.UUID
             A private instance variable that points to TID
-
-
-        Examples
-        --------
-        >>> isinstance(Task().TID, uuid.UUID)
-        True
         """
 
         self._task_id = uuid.uuid1()
+        self.name = name
 
     @property
     def TID(self) -> uuid.UUID:
+        """
+        The TID (task id) is public instance variable that
+        is accessed privately throuugh self._task_id
+
+        TID Datatype
+        ------------
+
+        >>> isinstance(Task(name='walk the dog').TID, uuid.UUID)
+        True
+        """
+
         return self._task_id
+
+    @property
+    def name(self) -> str:
+        """
+        The name of a task, which can be accessed simply as self.task
+
+        Constraints
+        -----------
+
+        1. Cannot exceed 15 characters:
+        >>> Task(name="A long task name")
+        Traceback (most recent call last):
+            ...
+        Exception: The length of a task name cannot exceed 15 characters.
+
+        2. Cannot be empty:
+        >>> Task(name="")
+        Traceback (most recent call last):
+            ...
+        Exception: A task name cannot be empty.
+
+        """
+
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) > 15:
+            raise Exception("The length of a task name cannot exceed 15 characters.")
+        if not len(value):
+            raise Exception("A task name cannot be empty.")
+
+        self._name = value
